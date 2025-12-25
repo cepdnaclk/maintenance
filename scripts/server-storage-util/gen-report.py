4c2c0423b9d42026fe9ed4449260efe72ba5890e
Gihan Jayatilaka <gihan@umd.edu>
2025-12-24 19:15:10 -0500

    Add files via upload

--- scripts/server-storage-util/gen-report.py
+++ scripts/server-storage-util/gen-report.py
@@ -14,19 +14,26 @@ fOut.write('''
 <title>Server Storage Usage</title>
 </head>
 <body>
+<h3><a href="https://www.pdn.ac.lk/">University of Peradeniya</a>: <a href="https://www.ce.pdn.ac.lk/">Department of Computer Engineering: <a href="https://cepdnaclk.github.io/maintenance/">Server Usage</a> :</h3>
 <h1>Server Storage Usage</h1>
-<p>This table shows the usage reports for folders that are larger than 10G on the Turing server.</p>
-
+<hr>
+<p>This table shows the usage reports for folders that are larger than 10G on CO Department server.</p>
+<h3 id="listOfServers">List of servers</h3>
 ''')
+
+for ssIdx,ss in enumerate(["kepler", "turing", "ampere", "babbage"]):
+    fOut.write("{}. <a href=\"#{}\">{}</a><br>\n".format(ssIdx+1,ss,ss))
 
 for ss in ["kepler", "turing", "ampere", "babbage"]:
     ff = "../../reports/server-storage-util/logs/{}-storage.log".format(ss)
     fIn = open(ff,"r")
 
     lines = fIn.readlines()
-    fOut.write("<b>{}</b> Read on: </td><td>".format(ss)+lines[0]+"</td></tr><br><br>\n")
+    fOut.write("<br><br><h3 id=\"{}\">{}</h3> Read on: {}.\n".format(ss,ss,lines[0]))
 
-
+    if ss == "babbage":
+        fOut.write("<p><span style=\"background: orange\">ORANGE = Alumni using more than 10GB</span></p>")
+        fOut.write("<p><span style=\"background: yellow\">YELLOW = Students using more than 50GB</span></p>")
 
     fOut.write('''
     <table border="1" style="border-collapse:collapse;">
@@ -55,10 +62,10 @@ for ss in ["kepler", "turing", "ampere", "babbage"]:
         if ss=="babbage":
             studentID = folder.split("/")[3]
             print(studentID)
-            if studentID[0:3] in ["e"+str(x) for x in range(0,17+1)]:
+            if studentID[0:3] in ["e"+str(x) for x in range(0,19+1)]:
                 fOut.write("<td bgcolor=\"orange\"><a href=\"https://people.ce.pdn.ac.lk/students/e{}/{}/\">profile</a></td>"\
                            .format(studentID[1:3],studentID[3:6]))
-            elif studentID[0]=="e" and studentID[1:].isdigit() and int(usageGB[:-1])>100:
+            elif studentID[0]=="e" and studentID[1:].isdigit() and int(usageGB[:-1])>50:
                 fOut.write("<td bgcolor=\"yellow\"><a href=\"https://people.ce.pdn.ac.lk/students/e{}/{}/\">profile</a></td>"\
                            .format(studentID[1:3],studentID[3:6]))
             else:
@@ -77,6 +84,9 @@ for ss in ["kepler", "turing", "ampere", "babbage"]:
 
 fOut.write('''
 <p>This page was last updated on {}.</p>
+<hr>
+This source code for generating this report is available <a href="https://github.com/cepdnaclk/maintenance">here</a>.<br>
+<a href="https://www.cs.umd.edu/~gihan/contact/">Contact Gihan via email</a> for all questions about this report. 
 
 </body>
 </html>
